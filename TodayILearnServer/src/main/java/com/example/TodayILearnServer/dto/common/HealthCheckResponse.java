e80b9b653208c2aad3e222b242dcc2d04ef81f8a
JIEUNI <jieundaeun@naver.com>
2024-05-16 13:19:18 +0900

    [TIL] Implement Health Check API

---
+++ TodayILearnServer/src/main/java/com/example/TodayILearnServer/dto/common/HealthCheckResponse.java
@@ -0,0 +1,15 @@
+package com.example.TodayILearnServer.dto.common;
+
+import lombok.Data;
+
+// API 응답 객체 정의
+@Data // lombok으로 사용할 수 있는 Data 어노테이션(getter/setter, 기본 생성자, toString())
+// 여기서는 @Getter만 사용해도 작동함.
+public class HealthCheckResponse {
+    private static final String STATUS = "OK";
+    private String status;
+
+    public HealthCheckResponse() {
+        this.status = STATUS;
+    }
+}
