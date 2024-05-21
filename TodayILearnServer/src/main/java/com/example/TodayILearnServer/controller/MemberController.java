0d477ab6efde2ed00a6ffb8c91016f8e51ac4ab6
JIEUNI <jieundaeun@naver.com>
2024-05-21 21:38:23 +0900

    [TIL] Controller : MemberController 생성

---
+++ TodayILearnServer/src/main/java/com/example/TodayILearnServer/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.example.TodayILearnServer.controller;
+
+import com.example.TodayILearnServer.dto.request.MemberCreateRequest;
+import com.example.TodayILearnServer.dto.response.MemberGetResponse;
+import com.example.TodayILearnServer.dto.request.MemberProfileUpdateRequest;
+import com.example.TodayILearnServer.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    // 특정 사용자 정보 단건 조회 v1
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberGetResponse> getMemberProfileV1(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.getMemberByIdV2(memberId));
+    }
+
+    // 특정 사용자 정보 단건 조회 v2
+    @GetMapping(value = "/{memberId}/v2", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MemberGetResponse> getMemberProfileV2(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.getMemberByIdV2(memberId));
+    }
+
+    // 생성
+    @PostMapping
+    public ResponseEntity<Void> createMember(@RequestBody MemberCreateRequest request) {
+        URI location =  URI.create(memberService.create(request));
+        return ResponseEntity.created(location).build();
+    }
+}
