a94c7728b2645a2ff9547d91614aba81135f81ca
JIEUNI <jieundaeun@naver.com>
2024-06-05 16:36:31 +0900

    [TIL] BaseTimeEntity 생성

---
+++ TodayILearnServer/src/main/java/com/example/TodayILearnServer/domain/Post/BaseTimeEntity.java
@@ -0,0 +1,20 @@
+package com.example.TodayILearnServer.domain.Post;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+// Database의 각 Table에 공통적으로 넣는 Column을 추상화하기 위한 클래스
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
